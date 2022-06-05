50283ca3ed8a5277d3ba0d006eedda1ac887989b
Tom Stovall <119924+stovak@users.noreply.github.com>
2022-06-05 11:54:47 -0700

    adding repo clone

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+if [[ -n "${GITCLONE_URL}" ]]; then
+  cd /var
+  rm -Rf /var/www
+  ## URL should look like this: https://${TOKEN}:x-oauth-basic@github.com/MyUser/MyRepo.git
+  git clone "${GITCLONE_URL}" www
+fi
 
 php-fpm
